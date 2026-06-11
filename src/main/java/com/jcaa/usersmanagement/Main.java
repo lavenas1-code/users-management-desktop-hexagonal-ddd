aa0e310288b84648c439fbbeaf8cfcf749e125d2
Luis Avena Suarez <lavenas1@unicartagena.edu.co>
2026-06-10 19:59:53 -0500

    refactor: actualizar Main para incluir modulo muestra

---
+++ src/main/java/com/jcaa/usersmanagement/Main.java
@@ -0,0 +1,25 @@
+package com.jcaa.usersmanagement;
+
+import com.jcaa.usersmanagement.infrastructure.config.DependencyContainer;
+import com.jcaa.usersmanagement.infrastructure.entrypoint.desktop.cli.UserManagementCli;
+import com.jcaa.usersmanagement.infrastructure.entrypoint.desktop.cli.io.ConsoleIO;
+import java.util.Scanner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class Main {
+
+  private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+  public static void main(final String[] args) {
+    log.info("Starting Users Management System...");
+    final DependencyContainer container = new DependencyContainer();
+    try (final Scanner scanner = new Scanner(System.in)) {
+      new UserManagementCli(
+              container.userController(),
+              container.muestraController(),
+              new ConsoleIO(scanner, System.out))
+          .start();
+    }
+  }
+}
